888bc8dce95648ff4d193b2cbe92c2bfc05258c1
Błażej Gajewski <gaju@MacBook-Pro-Bazej.local>
2025-06-03 13:05:27 +0200

    Add deploy config

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-primary">Tytuł strony</h1>
+      <button
+        className="bg-blue-300 p-4 mt-4 rounded"
+        onClick={() => setCount((c) => c + 1)}
+      >
+        count is {count}
+      </button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+  )
+}
---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e90ff", // wybierz swój kolor
+      },
+    },
+  },
+  plugins: [],
+}
